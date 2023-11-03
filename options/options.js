8385019fe29e59f79ac64f6d9968057285e6d2de
cb-abd-dev <cb@abd.dev>
2023-11-03 13:28:13 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "abd-231102-s1-dev-126419",
   url: "https://abd-231102-s1-dev-126419.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
